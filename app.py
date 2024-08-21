a777b75e39e3d2f15d8019d7bb5a919b3f216153
Muhyiddin Syarif <82252062+YI-SYA@users.noreply.github.com>
2024-08-21 16:12:34 +0700

    Update app.py

--- app.py
+++ app.py
@@ -21,16 +21,16 @@ def final_prediction(values, model):
 # Ini merupakan fungsi utama
 def main():
     
-    # Nilai awal
-    Cement = 540
-    Blast_slag = 142
-    Fly_ash = 163
-    Water = 205
-    Superplasticizer = 155
-    Fine_aggregate = 304
-    Age = 14
-    Coarse_aggregate = 938
-	
+    
+    Cement=540
+    Blast_slag=140
+    Fly_ash=0.5
+    Water=160
+    Superplasticizer=0.5
+    Coarse_aggregate=900
+    Fine_aggregate=600
+    Age=100
+    
     
     with st.container():
         col1, col2, col3 = st.columns(3)
@@ -39,35 +39,34 @@ def main():
         with col2:
             Blast_slag = st.number_input('Blast_slag', value=Blast_slag)
         with col3:
-            Fly_ash = st.number_input('Fly_ash', value=Fly_ash)
+           Fly_ash = st.number_input('Fly_ash', value=Fly_ash)
     with st.container():
-        col1, col2, col3 = st.columns(3)
-        with col1:
-            Water = st.number_input('Water', value=Water)
-        with col2:
-            Superplasticizer = st.number_input('Superplasticizer', value=Superplasticizer)
-        with col3:
-            Coarse_aggregate = st.number_input('Coarse_aggregate', value=Coarse_aggregate)
+         col1, col2, col3 = st.columns(3)
+         with col1:
+             Water = st.number_input('Water', value=Water)
+         with col2:
+             Superplasticizer = st.number_input('Superplasticizer', value=Superplasticizer)
+         with col3:
+            Coarse_aggregate = st.number_input('Coarse_aggregate', value=Coarse_aggregate)   
     with st.container():
-        col1, col2, col3 = st.columns(3)
+        col1, col2 = st.columns(2)
         with col1:
             Fine_aggregate = st.number_input('Fine_aggregate', value=Fine_aggregate)
         with col2:
-            Age = st.number_input('Age', value=Age)
-
+            Age = st.number_input('Age', value=Age)      
     
     st.markdown('---'*10)
-
+    
     
     data = {
         'Cement': Cement,
         'Blast_slag': Blast_slag,
         'Fly_ash': Fly_ash,
         'Water': Water,
-	'Superplasticizer' : Superplasticizer,
-	'Coarse_aggregate' : Coarse_aggregate,
-	'Fine_aggregate' : Fine_aggregate,
-	'Age' : Age,
+        'Superplasticizer': Superplasticizer,
+        'Coarse_aggregate': Coarse_aggregate,
+        'Fine_aggregate': Fine_aggregate,
+        'Age': Age,
         }
     
     kolom = list(data.keys())
@@ -75,14 +74,14 @@ def main():
     df_final = pd.DataFrame([data.values()],columns=kolom)
     
     # load model
-    my_model = pickle.load(open('model_oke.pkl', 'rb'))
+    my_model = pickle.load(open('model_regresi_terbaik.pkl', 'rb'))
     
     # Predict
     result = round(float(final_prediction(df_final, my_model)),2)
     
     st.markdown('---'*10)
     
-    st.write('<center><b><h3>Predicted = ', result,'</b></h3>', unsafe_allow_html=True)
+    st.write('<center><b><h3>Predicted= ', result,'</b></h3>', unsafe_allow_html=True)
            
-if __name__ == '__main__':
-	main() 
+if _name_ == '_main_':
+	main()
